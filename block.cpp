fbddd83f89c8ff19dd7833207a2e6d2d5aee542d
KYHSGekkCode <jourhyang123@gmail.com>
2018-08-09 13:36:47 +0900

    Images for textures

--- block.cpp
+++ block.cpp
@@ -1,6 +1,7 @@
 //block.cpp
 public Block::update()
 {
-	final float conductivity=this.rock.conductivity;
-	final float capacity=this.rock.capacity;
+	const float conductivity=this->rock->conductivity;
+    const float capacity=this->rock->capacity;
+    
 }
