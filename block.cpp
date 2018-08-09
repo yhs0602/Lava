d29cd6772ff169fee2743bcf793844b4d2c27b9f
KYHSGekkCode <jourhyang123@gmail.com>
2018-08-09 22:16:27 +0900

    added basic implementations

--- block.cpp
+++ block.cpp
@@ -3,9 +3,14 @@ void Block::update()
 {
 	const float conductivity=this->rock->conductivity;
     const float capacity=this->rock->capacity;
-    
+    float q=calculateHeats();
+    float dT=q/capacity;
+    temperature+=dT;
 }
 
-B lock::Block(Rock* rck)
-  {
-  }
+Block::Block(Rock* rck)
+{
+	this->rock=rck;
+	this->temperature=1000; //K
+	this->solid=false;
+}
--- rock.hpp
+++ rock.hpp
@@ -1,11 +1,17 @@
-//block.h
-#ifndef ROCK_H
-#define ROCK_H
+//rock.hpp
+#ifndef ROCK_HPP
+#define ROCK_HPP
 class Rock
 {
 	public:
-		Rock();
+		Rock(char* nam, float dens, float melting, float conduct, float capa);
 		float meltingPoint;
-		
-}
+		float density;
+		float conductivity;
+		float capacity;
+		char * name;
+		static Rock *air;
+		static Rock* rigid;
+};
+
 #endif
